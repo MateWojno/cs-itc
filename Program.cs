9a9e218bc192a56cf35d3b6b5153b46a212918ad
MateWojno <mateusz.k.wojno@gmail.com>
2022-10-25 15:50:49 +0200

    Exercise 1 done;
    It may need input validation inside while loop;

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Mateusz K Wojno
+Console.WriteLine("Rozwiazanie zadania numer 1");
+Console.WriteLine("Aby zakonczyc dzialanie programu nacisnij ctrl+c w terminalu");
+
+while (true)
+{
+Console.WriteLine("Podaj swoj wiek:");
+string userAge_str = Console.ReadLine();
+int userAge = Convert.ToInt16(userAge_str);      
+if (userAge  <18)
+{
+    Console.WriteLine("Jestes za mlody");
+}
+else if (userAge >= 18 && userAge < 70)
+{
+    Console.WriteLine("Mozesz wejsc");
+}
+else 
+{
+    Console.WriteLine("Jestes za stary");    
+}
+}
+
+
+
+

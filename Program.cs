0a2f817689d261c7cab8ffd623f8111ed4970f67
MateWojno <mateusz.k.wojno@gmail.com>
2022-10-25 16:00:29 +0200

    Exercise No. 2 done.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,45 @@
 ﻿// Mateusz K Wojno
-Console.WriteLine("Rozwiazanie zadania numer 1");
-Console.WriteLine("Aby zakonczyc dzialanie programu nacisnij ctrl+c w terminalu");
+Console.WriteLine("Aby uruchomic dany program prosze usunac komentarze");
 
-while (true)
-{
-Console.WriteLine("Podaj swoj wiek:");
-string userAge_str = Console.ReadLine();
-int userAge = Convert.ToInt16(userAge_str);      
-if (userAge  <18)
-{
-    Console.WriteLine("Jestes za mlody");
-}
-else if (userAge >= 18 && userAge < 70)
-{
-    Console.WriteLine("Mozesz wejsc");
-}
-else 
-{
-    Console.WriteLine("Jestes za stary");    
-}
-}
+// Console.WriteLine("Rozwiazanie zadania numer 1");
+// Console.WriteLine("Aby zakonczyc dzialanie programu nacisnij ctrl+c w terminalu");
+
+// while (true)
+// {
+// Console.WriteLine("Podaj swoj wiek:");
+// string userAge_str = Console.ReadLine();
+// int userAge = Convert.ToInt16(userAge_str);      
+// if (userAge  <18)
+// {
+//     Console.WriteLine("Jestes za mlody");
+// }
+// else if (userAge >= 18 && userAge < 70)
+// {
+//     Console.WriteLine("Mozesz wejsc");
+// }
+// else 
+// {
+//     Console.WriteLine("Jestes za stary");    
+// }
+// } 
 
 
+// Console.WriteLine("Rozwiazanie zadania numer 2");
+// Console.WriteLine("Podaj pierwszy odcinek: ");
+//     string firstNum_str = Console.ReadLine();
+//     int firstNum = Convert.ToInt16(firstNum_str);
+// Console.WriteLine("Podaj drugi odcinek: ");
+//     string secondNum_str = Console.ReadLine();
+//     int secondNum = Convert.ToInt16(secondNum_str);
+// Console.WriteLine("Podaj trzeci odcinek: ");
+//     string thirdNum_str = Console.ReadLine();
+//     int thirdNum = Convert.ToInt16(thirdNum_str);
 
-
+// if (firstNum + secondNum < thirdNum)
+// {
+//     Console.WriteLine("Z tych odcinkow mozna zbudowac trojkat");
+// }
+// else 
+// {
+//     Console.WriteLine("Z tych odcinkow nie zbudujesz trojkata");
+// }
